ba87d886c997e31fc2e0e0d26cc61d1e73994d3e
RenardS2 <mskim9625@gmail.com>
2025-09-05 17:39:26 +0900

    Commented entire Shop class due to build Error at Shop.

--- Shop.cpp
+++ Shop.cpp
@@ -1,40 +1,40 @@
-﻿#include "Shop.h"
-#include "Character.h"
-#include "Item.h"
-#include "HealthPotion.h"
-#include "AttackBoost.h"
-
-Shop::Shop()
-{
-	stock_.push_back(new HealthPotion());
-	stock_.push_back(new AttackBoost());
-}
-
-void Shop::displayItems() const
-{
-    for (int i = 0; i < stock_.size(); i++)
-    {
-        stock_[i]->getName();
-        stock_[i]->getPrice();
-    }
-}
-
-void Shop::buyItem(int index, Character & player)
-{
-    if (player.getGold() >= (*stock_[index]).getPrice())
-    {
-        player.addItem(*stock_[index]);
-        player.addGold(stock_[index]->getPrice());
-    }
-}
-
-int Shop::sellItem(int index, vector<Item>& invantory)
-{
-    for (int i = 0; i < stock_.size(); i++)
-    {
-        if (invantory[index].getName() == stock_[i]->getName())
-        {
-            return stock_[i]->getPrice();
-        }
-    }
-}
+﻿//#include "Shop.h"
+//#include "Character.h"
+//#include "Item.h"
+//#include "HealthPotion.h"
+//#include "AttackBoost.h"
+//
+//Shop::Shop()
+//{
+//	stock_.push_back(new HealthPotion());
+//	stock_.push_back(new AttackBoost());
+//}
+//
+//void Shop::displayItems() const
+//{
+//    for (int i = 0; i < stock_.size(); i++)
+//    {
+//        stock_[i]->getName();
+//        stock_[i]->getPrice();
+//    }
+//}
+//
+//void Shop::buyItem(int index, Character & player)
+//{
+//    if (player.getGold() >= (*stock_[index]).getPrice())
+//    {
+//        player.addItem(*stock_[index]);
+//        player.addGold(stock_[index]->getPrice());
+//    }
+//}
+//
+//int Shop::sellItem(int index, vector<Item>& invantory)
+//{
+//    for (int i = 0; i < stock_.size(); i++)
+//    {
+//        if (invantory[index].getName() == stock_[i]->getName())
+//        {
+//            return stock_[i]->getPrice();
+//        }
+//    }
+//}
