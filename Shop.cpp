98c8651d89c45558eb369f819ac663a14b76f764
jungmin001 <jungmin4556@naver.com>
2025-09-08 13:07:48 +0900

    Shop

--- Shop.cpp
+++ Shop.cpp
@@ -3,6 +3,9 @@
 #include "Item.h"
 #include "HealthPotion.h"
 #include "AttackBoost.h"
+#include <iostream>
+
+using namespace std;
 
 Shop::Shop()
 {
@@ -14,8 +17,8 @@ void Shop::displayItems() const
 {
     for (int i = 0; i < stock_.size(); i++)
     {
-        stock_[i]->getName();
-        stock_[i]->getPrice();
+		cout << stock_[i]->getName() << " - " 
+             << stock_[i]->getPrice() << "Gold\n" << endl;
     }
 }
 
@@ -23,18 +26,45 @@ void Shop::buyItem(int index, Character* player)
 {
     if (player->getGold() >= (*stock_[index]).getPrice())
     {
-        player->addItem(stock_[index]);
-        player->addGold(stock_[index]->getPrice());
+		Item* item = nullptr;
+
+        switch (index)
+        {
+            case 0:
+				item = new HealthPotion();
+                break;
+			case 1:
+                item = new AttackBoost();
+				break;
+            default:
+                cout << "Invalid item Item Number.\n";
+				return;
+        }
+		player->addItem(item);
+
+        //player->addItem(stock_[index]);
+        player->spendGold(stock_[index]->getPrice());
+        cout << "Purchased complete!\n"
+            << "Gold Remaining : " << player->getGold();
+    }
+    else
+    {
+        cout << "Not enough Gold.\n"
+            << "Gold Remaining : " << player->getGold();
     }
 }
 
 int Shop::sellItem(int index, vector<Item*> invantory)
 {
-    for (int i = 0; i < stock_.size(); i++)
-    {
-        if (invantory[index]->getName() == stock_[i]->getName())
+        for (int i = 0; i < stock_.size(); i++)
         {
-            return stock_[i]->getPrice();
+            if (invantory[index]->getName() == stock_[i]->getName())
+            {
+                cout << "Sold " << invantory[index]->getName() << " - "
+                    << (stock_[i]->getPrice() * SELL_RATE_PERCENT) / 100 << " Gold \n";
+                return stock_[i]->getPrice();
+            }
         }
-    }
+		cout << "Item not found in shop.\n";
+return 0;
 }
